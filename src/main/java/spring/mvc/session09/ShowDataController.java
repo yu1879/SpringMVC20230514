4025ca69011582a945004fe9108c04d29664dca7
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-21 16:55:15 +0800

    mvc

---
+++ src/main/java/spring/mvc/session09/ShowDataController.java
@@ -0,0 +1,24 @@
+package spring.mvc.session09;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/showdata")
+public class ShowDataController {
+	@RequestMapping("/case1")
+
+	public ModelAndView case1() {
+		String data = new Date() + "";
+
+		String view = "clock.jsp";
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("data", data);
+		mav.setViewName(view);
+		return mav;
+	}
+}
---
+++ src/main/webapp/WEB-INF/clock.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Clock</title>
+</head>
+<body style="">
+	<img alt=""
+		src="${pageContext.request.servletContext }/images/clock.png">
+	<p>
+	<h1>現在時刻:${data}</h1>
+</body>
+</html>
