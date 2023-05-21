21821d9352c38b08e569821c6346685013a5c3dc
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-21 13:29:35 +0800

    mvc

---
+++ src/main/java/spring/mvc/session08/controller/HelloController.java
@@ -0,0 +1,16 @@
+package spring.mvc.session08.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+	@RequestMapping(value = "/welcome")
+	@ResponseBody
+	public String welcome() {
+		return "Welcome Spring MVC!";
+	}
+}
