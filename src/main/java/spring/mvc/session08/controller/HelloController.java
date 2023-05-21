1776afd57eae0e01ff4e51a2f8420a3dead179b5
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-21 14:37:49 +0800

    mvc

--- src/main/java/spring/mvc/session08/controller/HelloController.java
+++ src/main/java/spring/mvc/session08/controller/HelloController.java
@@ -1,5 +1,7 @@
 package spring.mvc.session08.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,10 +19,32 @@ public class HelloController {
 
 	@RequestMapping(value = "/sayhi")
 	@ResponseBody
-	public String sayHi(@RequestParam(value = "name") String name, @RequestParam(value = "age") Integer age) {
+	public String sayHi(@RequestParam(value = "name", required = true) String name,
+			@RequestParam(value = "age", required = false, defaultValue = "0") Integer age) {
 
 		String data = String.format("Hi %s, %d", name, age);
 		return data;
+	}
+
+//	@RequestMapping(value = "/bmi")
+//	@ResponseBody
+//	public Double bmi(@RequestParam(value = "height", required = true) Double h,
+//			@RequestParam(value = "weight", required = false, defaultValue = "0") Double w) {
+//		Double bmi = w / ((h / 100.0) * (h / 100.0));
+//		return bmi;
+//	}
+	@RequestMapping("/bmi")
+	@ResponseBody
+	public String bmi(@RequestParam("h") Double h, @RequestParam("w") Double w) {
+		double bmivalue = w / Math.pow(h / 100, 2);
+		return String.format("bmi=%.2f", bmivalue);
+	}
+
+	@RequestMapping("/age")
+	@ResponseBody
+	public String getAvgOfAge(@RequestParam("age") List<Integer> ages) {
+		double avg = ages.stream().mapToInt(Integer::intValue).average().getAsDouble();
+		return String.format("avg=%.1f", avg);
 	}
 
 }
