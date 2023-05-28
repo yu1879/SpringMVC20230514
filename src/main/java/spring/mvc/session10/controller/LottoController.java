b88304c67a3cf770927efdac059bddfb31e53554
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-28 15:17:37 +0800

    mvc

--- src/main/java/spring/mvc/session10/controller/LottoController.java
+++ src/main/java/spring/mvc/session10/controller/LottoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -35,6 +36,22 @@ public class LottoController {
 		Set<Integer> lotto = lottoService.add();
 		attr.addAttribute("lotto", lotto);
 		// attr.addFlashAttribute("lotto", lotto);
+		return "redirect:./";
+
+	}
+
+	@GetMapping("/update/{index}")
+	public String update(@PathVariable("index") int index) {
+
+		lottoService.update(index); // attr.addFlashAttribute("lotto", lotto);
+		return "redirect:./";
+
+	}
+
+	@GetMapping("/delete/{index}")
+	public String delete(@PathVariable("index") int index) {
+
+		lottoService.delete(index); // attr.addFlashAttribute("lotto", lotto);
 		return "redirect:./";
 
 	}
