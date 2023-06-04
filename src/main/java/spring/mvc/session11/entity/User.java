01cc3a8350678c90a83e3559ddfeefe845aa48a7
yu1879 <shengweiyukobe0526@gmail.com>
2023-06-04 13:21:45 +0800

    mvc

--- src/main/java/spring/mvc/session11/entity/User.java
+++ src/main/java/spring/mvc/session11/entity/User.java
@@ -1,5 +1,6 @@
 package spring.mvc.session11.entity;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -12,5 +13,86 @@ public class User {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@JsonFormat
 	private Date birth;
+	private String education;
+	private String sex;
+	private String[] interest;
+	private String resume;
+
+	public User() {
+		super();
+	}
+
+	public User(String name, Integer age, Date birth, String education, String sex, String[] interest, String resume) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.birth = birth;
+		this.education = education;
+		this.sex = sex;
+		this.interest = interest;
+		this.resume = resume;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public String getEducation() {
+		return education;
+	}
+
+	public void setEducation(String education) {
+		this.education = education;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String[] getInterest() {
+		return interest;
+	}
+
+	public void setInterest(String[] interest) {
+		this.interest = interest;
+	}
+
+	public String getResume() {
+		return resume;
+	}
+
+	public void setResume(String resume) {
+		this.resume = resume;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + ", birth=" + birth + ", education=" + education + ", sex=" + sex
+				+ ", interest=" + Arrays.toString(interest) + ", resume=" + resume + "]";
+	}
 
 }
