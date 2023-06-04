c34b6d72d7c9bebbeb33a34df0eab7a794ec5036
yu1879 <shengweiyukobe0526@gmail.com>
2023-06-04 15:05:37 +0800

    mvc

--- src/main/java/spring/mvc/session11/controller/UserController.java
+++ src/main/java/spring/mvc/session11/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session11.entity.User;
@@ -30,4 +31,11 @@ public class UserController {
 		model.addAttribute("users", users);
 		return "session11/user";
 	}
+
+	@PostMapping("/")
+	public String add(@ModelAttribute User user) {
+		users.add(user);
+		return "redirect./";
+	}
+
 }
