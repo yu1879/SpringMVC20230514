ad87253c5c92d84967552bdeefd4ec3ad97a2d7a
yu1879 <shengweiyukobe0526@gmail.com>
2023-06-18 15:07:34 +0800

    jdbc

--- src/main/java/spring/mvc/session15/controller/JobController.java
+++ src/main/java/spring/mvc/session15/controller/JobController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session15.entity.Job;
@@ -27,6 +28,19 @@ public class JobController {
 	public String index(@ModelAttribute Job job, Model model) {
 		model.addAttribute("_method", "POST");
 		model.addAttribute("jobs", jobDao.query());
+		model.addAttribute("pageCount", getPageCount());
+
+		return "session15/job";
+	}
+
+	@GetMapping("/page/{pageNo}")
+	public String page(@PathVariable("pageNo") int pageNo, @ModelAttribute Job job, Model model) {
+		if (pageNo < 0) {
+			return "redirect:../";
+		}
+
+		model.addAttribute("_method", "POST");
+		model.addAttribute("jobs", jobDao.queryPage(pageNo));
 		model.addAttribute("pageCount", getPageCount());
 
 		return "session15/job";
