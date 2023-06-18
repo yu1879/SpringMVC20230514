9ac570213f2d3a48f03d13535588c8ce5d6b3104
yu1879 <shengweiyukobe0526@gmail.com>
2023-06-18 13:45:14 +0800

    mvc

--- src/main/java/spring/mvc/session15/entity/Job.java
+++ src/main/java/spring/mvc/session15/entity/Job.java
@@ -1,5 +1,46 @@
 package spring.mvc.session15.entity;
 
 public class Job {
+	private Integer jid;
+	private String jname;
+	private Integer eid;
+	private Employee employee;
+
+	public Integer getJid() {
+		return jid;
+	}
+
+	public void setJid(Integer jid) {
+		this.jid = jid;
+	}
+
+	public String getJname() {
+		return jname;
+	}
+
+	public void setJname(String jname) {
+		this.jname = jname;
+	}
+
+	public Integer getEid() {
+		return eid;
+	}
+
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	@Override
+	public String toString() {
+		return "Job [jid=" + jid + ", jname=" + jname + ", eid=" + eid + ", employee=" + employee + "]";
+	}
 
 }
