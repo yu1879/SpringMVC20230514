173e481ea86f2d408c850fffa7866b2073f3b795
yu1879 <shengweiyukobe0526@gmail.com>
2023-06-18 13:39:15 +0800

    mvc

---
+++ src/main/java/spring/mvc/session15/entity/Employee.java
@@ -0,0 +1,74 @@
+package spring.mvc.session15.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class Employee {
+
+	private Integer id;
+	@Size(min = 2, max = 50, message = "{employee.ename.size}")
+	private String ename;
+	@NotNull(message = "{employee.salary.empty}")
+	@Range(min = 30000, max = 300000, message = "{employee.salary.range}")
+	private Integer salary;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date createtime;
+
+	private List<Job> jobs;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	public List<Job> getJobs() {
+		return jobs;
+	}
+
+	public void setJobs(List<Job> jobs) {
+		this.jobs = jobs;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", ename=" + ename + ", salary=" + salary + ", createtime=" + createtime
+				+ ", jobs=" + jobs + "]";
+	}
+
+}
