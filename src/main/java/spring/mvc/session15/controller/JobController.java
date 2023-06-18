ee43a2dcb9d4d47a127cd6ca7da7898ebb3fa5d0
yu1879 <shengweiyukobe0526@gmail.com>
2023-06-18 16:41:22 +0800

    jdbc

--- src/main/java/spring/mvc/session15/controller/JobController.java
+++ src/main/java/spring/mvc/session15/controller/JobController.java
@@ -1,13 +1,16 @@
 package spring.mvc.session15.controller;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session15.entity.Job;
@@ -61,6 +64,21 @@ public class JobController {
 
 		return "session15/job";
 
+	}
+
+	@PostMapping("/")
+	public String add(@ModelAttribute @Valid Job job, BindingResult result, Model model, HttpSession session) {
+		if (result.hasErrors()) {
+			model.addAttribute("_method", "POST");
+			setBaseModelAttribute(model, session);
+			return "sessio15/job";
+		}
+		jobDao.add(job);
+		String sessionNum = session.getAttribute("num") + "";
+		if (sessionNum.length() > 0) {
+			session.setAttribute("num", getPageCount());
+		}
+		return "redirect:./";
 	}
 
 	private void setBaseModelAttribute(Model model, HttpSession session) {
