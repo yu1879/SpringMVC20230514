00494bc6a9412da5d9fa3258872bd2312c09f72c
yu1879 <shengweiyukobe0526@gmail.com>
2023-07-16 17:21:02 +0800

    exception

--- src/main/java/spring/mvc/session17/controller/DivisionController.java
+++ src/main/java/spring/mvc/session17/controller/DivisionController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/session17/divisin")
+@RequestMapping("/session17/division")
 public class DivisionController {
 
 }
